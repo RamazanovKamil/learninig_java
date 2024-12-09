1a73f71e7c8bca376d2fa6772c0ddd097c551477
KamilR <runner27la@gmail.com>
2024-12-09 21:08:16 +0300

    Calc average with double casting

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,8 +1,10 @@
 public class MainClass{
     public static void main(){
-        print();
-    }
-    static void print(){
-        System.out.println("Hello world 5D !");
+        int num1 = 12;
+        int num2 = -5;
+        int num3 = -6;
+        int num4 = 50;
+        double avg = (double)(num1 + num2 + num3 + num4) / 4;
+        System.out.println(avg);
     }
 }
